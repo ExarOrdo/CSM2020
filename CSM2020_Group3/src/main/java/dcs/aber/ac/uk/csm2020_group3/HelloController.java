12bff851a92f44f9cc6372154f70af1acdd131ee
Tom <124250229+damnlookatit@users.noreply.github.com>
2023-02-09 10:42:58 +0000

    javafx version

---
+++ CSM2020_Group3/src/main/java/dcs/aber/ac/uk/csm2020_group3/HelloController.java
@@ -0,0 +1,14 @@
+package dcs.aber.ac.uk.csm2020_group3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
