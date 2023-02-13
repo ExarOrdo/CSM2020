8fd318316e43cf1b7e7ebb153e76dae6df5b3723
tjw20 <tjw20@aber.ac.uk>
2023-02-13 13:06:00 +0000

    update tjw20test

---
+++ CSM2020_Group3/src/main/java/dcs/aber/ac/uk/csm2020_group3/RegisterController.java
@@ -0,0 +1,18 @@
+package dcs.aber.ac.uk.csm2020_group3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterController {
+    @FXML
+    ComboBox yearComboBox;
+
+    @FXML
+    public void initialize() {
+        yearComboBox.getItems().removeAll(yearComboBox.getItems());
+        yearComboBox.getItems().addAll("0", "1", "2", "3");
+    }
+}
