298079faa8d6fb0443abd635dc2803e61e21273a
Lars <lars2000ad@gmail.com>
2023-02-13 17:44:05 +0000

    Delete csm2020_Group_3 directory

--- csm2020_Group_3/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module dcs.aber.ac.uk.csm2020_group_3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens dcs.aber.ac.uk.csm2020_group_3 to javafx.fxml;
-    exports dcs.aber.ac.uk.csm2020_group_3;
-}
