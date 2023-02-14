baecf48a0d29f5d8b0b62392b0e97a64b803d83c
Lars <lars2000ad@gmail.com>
2023-02-14 14:54:30 +0000

    Update Branch

--- src/main/java/dcs/aber/ac/uk/csm2020_group_3/Main.java
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 
 
 
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
