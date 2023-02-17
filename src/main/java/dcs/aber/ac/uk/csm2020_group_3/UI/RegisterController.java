5cf798537121c1553458d7217c76919ebddaa896
tjw20 <tjw20@aber.ac.uk>
2023-02-17 11:21:21 +0000

    UI changes

--- src/main/java/dcs/aber/ac/uk/csm2020_group_3/UI/RegisterController.java
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/UI/RegisterController.java
@@ -1,8 +1,11 @@
 package dcs.aber.ac.uk.csm2020_group_3.UI;
 
+import dcs.aber.ac.uk.csm2020_group_3.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,5 +17,21 @@ public class RegisterController {
     public void initialize() {
         yearComboBox.getItems().removeAll(yearComboBox.getItems());
         yearComboBox.getItems().addAll("0", "1", "2", "3");
+    }
+
+    @FXML
+    private void cancel(ActionEvent actionEvent) throws IOException {
+
+        Main main = new Main();
+        main.changeScene("Login.fxml");
+
+    }
+
+    @FXML
+    private void confirm(ActionEvent actionEvent) throws IOException {
+
+        Main main = new Main();
+        main.changeScene("Recommendation.fxml");
+
     }
 }
