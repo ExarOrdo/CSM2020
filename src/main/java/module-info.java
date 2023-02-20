6d51caf9d7bd3f8cd0237109743b0fcc59483961
hugok <hdk2@aber.ac.uk>
2023-02-20 17:49:31 +0000

    DB Proto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module dcs.aber.ac.uk.csm2020_group_3 {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.sql;
 
     opens dcs.aber.ac.uk.csm2020_group_3 to javafx.fxml;
     exports dcs.aber.ac.uk.csm2020_group_3;
