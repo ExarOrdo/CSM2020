02b54246d0a6e1a6f28ee4a3ccefd2745070c6a6
damnlookatit <thomasjwaite12@gmail.com>
2023-04-03 14:53:44 +0100

    Admin login UI updates

---
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/UI/AdminTimetableController.java
@@ -0,0 +1,51 @@
+package dcs.aber.ac.uk.csm2020_group_3.UI;
+
+import dcs.aber.ac.uk.csm2020_group_3.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class AdminTimetableController {
+    @FXML
+    Pane expandedPane;
+    @FXML
+    Button burgerBtn;
+    @FXML
+    Button logoutBtn;
+    @FXML
+    ImageView burgerIcon;
+    @FXML
+    Button adminTimetableBtn;
+    @FXML
+    Button adminBtn;
+
+    @FXML
+    protected void pressBurgerBtn() {
+        expandedPane.setVisible(!expandedPane.isVisible());
+        if(expandedPane.isVisible()){
+            burgerIcon.setRotate(90);
+        }else {
+            burgerIcon.setRotate(0);
+        }
+    }
+
+    @FXML
+    private void logout() throws IOException {
+        Main main = new Main();
+        main.changeScene("Login.fxml");
+    }
+
+    @FXML
+    private void toAdminTimetable(){
+        pressBurgerBtn();
+    }
+
+    @FXML
+    private void toAdmin() throws IOException {
+        Main main = new Main();
+        main.changeScene("Admin.fxml");
+    }
+}
