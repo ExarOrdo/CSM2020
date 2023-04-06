9c05cac56778419ff2c266b40c7a46421b883a96
Bartosz Olewinski <bartosz.olewinski@gmail.com>
2023-04-06 14:26:59 +0200

    Added missing changes from previous commit

--- src/main/java/dcs/aber/ac/uk/csm2020_group_3/DatabaseHandler/RecordCreator.java
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/DatabaseHandler/RecordCreator.java
@@ -3,6 +3,9 @@ package dcs.aber.ac.uk.csm2020_group_3.DatabaseHandler;
 import dcs.aber.ac.uk.csm2020_group_3.RecordTypes.*;
 import dcs.aber.ac.uk.csm2020_group_3.RecordTypes.Record;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * Creates a record for a new user
  * might need marks??? modules????
@@ -18,17 +21,63 @@ public class RecordCreator extends DatabaseHandler{
         this.typeOfRecord = recordObject.getTableName();
     }
 
-    public boolean tryCreatingRecord() {
+    public boolean tryCreatingRecord() throws SQLException {
 
 
-
+        this.connection = getConnection();
 
         //apparently switch Pattern Matching is not supported in java 18 so enjoy extra variable band-aid
         switch (typeOfRecord) {
+            //case of registering a student
             case STUDENT:
+                //cast recordObject into StudentRecord object
+                StudentRecord studentRecord = (StudentRecord) recordObject;
+
+                try{
+                    PreparedStatement createStudent = connection.prepareStatement("INSERT INTO STUDENT (StudentID,StudentName,StudentCourse,StudentYear,StudentPassword) VALUES (?,?,?,?,?)");
+
+                    createStudent.setString(1, studentRecord.getStudentId());
+                    createStudent.setString(2,studentRecord.getStudentName());
+                    createStudent.setString(3, studentRecord.getStudentCourse());
+                    createStudent.setInt(4, studentRecord.getStudentYear());
+                    createStudent.setString(5,studentRecord.getStudentPassword());
+
+                    createStudent.execute();
+                    createStudent.close();
+
+                    System.out.println("Registered student with id: " + studentRecord.getStudentId());
+
+
+                }catch  (Exception err) {
+                    System.err.println("Error when creating Student record in Student table:" + err.getMessage());
+                    err.printStackTrace();
+                    return false;
+                }
 
                 break;
             case OPTIONAL_MODULE:
+
+                OptionalModuleRecord optionalModuleRecord = (OptionalModuleRecord) recordObject;
+
+                this.connection = getConnection();
+                try{
+                    PreparedStatement createRecord = connection.prepareStatement("INSERT INTO OPTIONAL_MODULE (CousreID, ModuleID) VALUES (?,?)");
+
+                    createRecord.setString(1, optionalModuleRecord.getCourseId());
+                    createRecord.setString(2,optionalModuleRecord.getModuleId());
+
+                    createRecord.execute();
+                    createRecord.close();
+
+                    System.out.println("Added new optional module with id: " + optionalModuleRecord.getModuleId() + " for " +
+                            "course with id: +" + optionalModuleRecord.getCourseId());
+
+
+                }catch  (Exception err) {
+                    System.err.println("Error when creating Optional module record in Optional_Module table:" + err.getMessage());
+                    err.printStackTrace();
+                    return false;
+                }
 
                 break;
             case CORE_MODULE:
@@ -45,7 +94,7 @@ public class RecordCreator extends DatabaseHandler{
                 break;
             default:
                 System.err.println("Unknown type of Record subclass instance type.");
-                break;
+                return false;
         }
 
 
--- src/main/java/dcs/aber/ac/uk/csm2020_group_3/DatabaseHandler/RecordRemover.java
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/DatabaseHandler/RecordRemover.java
@@ -1,29 +1,98 @@
 package dcs.aber.ac.uk.csm2020_group_3.DatabaseHandler;
 
-/**
- * Removes student records from the db
- */
+import dcs.aber.ac.uk.csm2020_group_3.RecordTypes.*;
+import dcs.aber.ac.uk.csm2020_group_3.RecordTypes.Record;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
 public class RecordRemover extends DatabaseHandler{
 
-    private String studentId;
+    private Record recordObject;
 
-    private String studentPassword;
+    private Table typeOfRecord;
+
 
     /**
-     *
-     * @param studentId id of the student being removed
-     * @param studentPassword student's password????
+     * Constructs RecordRemover for removing a record of specific Record subclass
+     * @param recordObject
      */
-    public RecordRemover(String studentId, String studentPassword) {
-        this.studentId = studentId;
+    public RecordRemover(Record recordObject) {
+        this.recordObject = recordObject;
 
-        this.studentPassword = studentPassword;
-
+        this.typeOfRecord = recordObject.getTableName();
     }
 
-    @Override
-    public void load() {;}
+    public boolean tryRemovingRecord() throws SQLException {
+        this.connection = getConnection();
 
-    @Override
-    public void remove() {;}
+        //enjoy another switch magic because no Pattern matching
+        switch (typeOfRecord) {
+            //case of registering a student
+            case STUDENT:
+                //cast recordObject into StudentRecord object
+                StudentRecord studentRecord = (StudentRecord) recordObject;
+
+                try {
+                    PreparedStatement createStudent = connection.prepareStatement("DELETE FROM STUDENT WHERE StudentID = ?");
+
+                    createStudent.setString(1, studentRecord.getStudentId());
+
+                    createStudent.execute();
+                    createStudent.close();
+
+                    System.out.println("Removed student with id: " + studentRecord.getStudentId());
+
+
+                } catch (Exception err) {
+                    System.err.println("Error when removing Student record from Student table:" + err.getMessage());
+                    err.printStackTrace();
+                    return false;
+                }
+
+                break;
+            case OPTIONAL_MODULE:
+
+                OptionalModuleRecord optionalModuleRecord = (OptionalModuleRecord) recordObject;
+
+                this.connection = getConnection();
+                try {
+                    PreparedStatement createRecord = connection.prepareStatement("DELETE FROM OPTIONAL_MODULE (CousreID, ModuleID) VALUES (?,?)");
+
+                    createRecord.setString(1, optionalModuleRecord.getCourseId());
+                    createRecord.setString(2, optionalModuleRecord.getModuleId());
+
+                    createRecord.execute();
+                    createRecord.close();
+
+                    System.out.println("Deleted optional module with id: " + optionalModuleRecord.getModuleId() + " for " +
+                            "course with id: +" + optionalModuleRecord.getCourseId());
+
+
+                } catch (Exception err) {
+                    System.err.println("Error when deleting Optional module record in Optional_Module table:" + err.getMessage());
+                    err.printStackTrace();
+                    return false;
+                }
+
+                break;
+            case CORE_MODULE:
+
+                break;
+            case MODULE:
+
+                break;
+            case MARKS:
+
+                break;
+            case COURSE:
+
+                break;
+            default:
+                System.err.println("Unknown type of Record subclass instance type.");
+                return false;
+        }
+        return true;
+    }
 }
