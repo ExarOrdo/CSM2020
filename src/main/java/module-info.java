3d8878dea8a0605ac72e29cb01b95445f1352360
hugok <hdk2@aber.ac.uk>
2023-04-17 07:05:21 +0100

    Student Record page

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module dcs.aber.ac.uk.csm2020_group_3 {
     requires org.controlsfx.controls;
     requires java.sql;
 
+    opens dcs.aber.ac.uk.csm2020_group_3.DatabaseHandler to javafx.base;
     opens dcs.aber.ac.uk.csm2020_group_3 to javafx.fxml;
     exports dcs.aber.ac.uk.csm2020_group_3;
     exports dcs.aber.ac.uk.csm2020_group_3.UI;
