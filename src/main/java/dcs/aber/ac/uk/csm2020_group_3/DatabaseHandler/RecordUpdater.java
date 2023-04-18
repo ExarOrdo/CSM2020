bbf06bd93584b5dba855a44c8e482ca2f6c57dc8
bao5 <Shorty*546>
2023-04-18 13:08:46 +0100

    Created some small details for RecordUpdater, will branch off from here

--- src/main/java/dcs/aber/ac/uk/csm2020_group_3/DatabaseHandler/RecordUpdater.java
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/DatabaseHandler/RecordUpdater.java
@@ -1,8 +1,30 @@
 package dcs.aber.ac.uk.csm2020_group_3.DatabaseHandler;
 
+import dcs.aber.ac.uk.csm2020_group_3.RecordTypes.Record;
+
 /**
  * Updates any specified record with new information
  */
 public class RecordUpdater extends DatabaseHandler{
+
+    private final Table typeOfRecord;
+
+    private final Record recordToUpdate;
+
+    private final Record newRecordDetails;
+
+    /**
+     * Constructs an Object that given an instance of Record's subclass, can update this record with new details
+     * @param recordObject record object to update
+     * @param newRecordDetails record object containing new details
+     */
+    public RecordUpdater(Record recordObject, Record newRecordDetails) {
+        this.typeOfRecord = recordObject.getTableName();
+
+        this.recordToUpdate = recordObject;
+
+        this.newRecordDetails = newRecordDetails;
+    }
+
 
 }
