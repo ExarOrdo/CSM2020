97e018795b80deee116063b066bc570ea3889dfd
tjw20 <tjw20@aber.ac.uk>
2023-04-18 15:22:34 +0100

    got list of modules for timetable

--- src/main/java/dcs/aber/ac/uk/csm2020_group_3/UI/TimetableController.java
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/UI/TimetableController.java
@@ -1,14 +1,20 @@
 package dcs.aber.ac.uk.csm2020_group_3.UI;
 
 import dcs.aber.ac.uk.csm2020_group_3.Main;
+import dcs.aber.ac.uk.csm2020_group_3.RecommendationSystem.CoreListGenerator;
+import dcs.aber.ac.uk.csm2020_group_3.RecommendationSystem.ElectiveListGenerator;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class TimetableController {
+public class TimetableController implements Initializable {
     @FXML
     Pane expandedPane;
     @FXML
@@ -25,6 +31,9 @@ public class TimetableController {
     Button helpBtn;
     @FXML
     Button timetableBtn;
+    private final ArrayList<String> electiveNames = new ArrayList<>();
+    private final ArrayList<String> coreNames = new ArrayList<>();
+
     @FXML
     protected void pressBurgerBtn() {
         expandedPane.setVisible(!expandedPane.isVisible());
@@ -70,5 +79,18 @@ public class TimetableController {
     @FXML
     private void toTimetable() {
         pressBurgerBtn();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        for (int i = 0; i < ElectiveListGenerator.electiveModulesList.size(); i++) {
+            electiveNames.add(ElectiveListGenerator.electiveModulesList.get(i).getName());
+        }
+        for (int i = 0; i < CoreListGenerator.coreModulesList.size(); i++) {
+            coreNames.add(CoreListGenerator.coreModulesList.get(i).getName());
+        }
+        System.out.println(electiveNames);
+        System.out.println(coreNames);
     }
 }
