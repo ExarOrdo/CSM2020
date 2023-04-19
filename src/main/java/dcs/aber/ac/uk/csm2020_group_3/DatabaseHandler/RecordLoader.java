42efad78e8d1edbd824e1d40428f66cfc28aa599
bao5 <Shorty*546>
2023-04-19 15:36:31 +0100

    Added Subjects to the ComboBox in Admin page along with its loading and small test

--- src/main/java/dcs/aber/ac/uk/csm2020_group_3/DatabaseHandler/RecordLoader.java
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/DatabaseHandler/RecordLoader.java
@@ -3,6 +3,7 @@ package dcs.aber.ac.uk.csm2020_group_3.DatabaseHandler;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class RecordLoader extends DatabaseHandler{
 
@@ -24,5 +25,28 @@ public class RecordLoader extends DatabaseHandler{
             err.printStackTrace();
         }
         return -1;
+    }
+
+    public ArrayList<String> getSubjectList() throws SQLException {
+        ArrayList<String> courseList = new ArrayList<>();
+
+        this.connection = getConnection();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT DISTINCT ModuleTag1 FROM MODULE");
+            ResultSet courseResultSet = statement.executeQuery();
+
+            while (courseResultSet.next()) {
+                courseList.add(courseResultSet.getString("ModuleTag1"));
+                System.out.println(courseResultSet.getString("ModuleTag1"));
+            }
+
+
+        } catch (Exception err) {
+            System.err.print("Error during getting subject list for admin page: " + err.getMessage());
+            err.printStackTrace();
+        }
+
+        return courseList;
     }
 }
--- src/main/java/dcs/aber/ac/uk/csm2020_group_3/UI/AdminController.java
+++ src/main/java/dcs/aber/ac/uk/csm2020_group_3/UI/AdminController.java
@@ -1,15 +1,26 @@
 package dcs.aber.ac.uk.csm2020_group_3.UI;
 
+import dcs.aber.ac.uk.csm2020_group_3.DatabaseHandler.RecordLoader;
 import dcs.aber.ac.uk.csm2020_group_3.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class AdminController {
+public class AdminController implements Initializable {
 
+    private RecordLoader recordLoader = new RecordLoader();
+
+    @FXML
+    ComboBox subjectBox;
     @FXML
     Pane expandedPane;
     @FXML
@@ -52,6 +63,21 @@ public class AdminController {
     @FXML
     private void toAdmin() {
         pressBurgerBtn();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ArrayList<String> subjectStringList = new ArrayList<>();
+        try {
+            subjectStringList = recordLoader.getSubjectList();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //populate combo box for subjects
+        for (int i = 0; i < subjectStringList.size(); i++) {
+            subjectBox.getItems().add(subjectStringList.get(i));
+        }
     }
 
 
